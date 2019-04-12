e8f1e68c22cb6d9b7040b3e231af1471cbfa8aa2
John Robertson <tiktaalikdesign@gmail.com>
2019-04-12 13:59:54 -0400

    progress on the dry api call method.  function works and we can pass in api params as arguments.  next step is to set up properties for each api key object to hold input frm the user (location, category, price, etc.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
+let apiResponse = [];
+
 $(document).ready(function () {
 
     // < --- ========== Begin API KEYS ========== --- >
     // placing our keys here so we can reuse ajax method
     let apiKeys = [
         {
-            api: 'GEOLOCATION',
+            api: 'geo',
             key: '8bf6f7f9f28c7ed5495f6f6353d07d28a05c6bb9fbab598f32e7cba3',
+            params: [],
             url: 'https://api.ipdata.co/?api-key=',
         },
         // extra  template api object
@@ -15,32 +18,65 @@ $(document).ready(function () {
         //     url: '',
         // },
         {
-            api: 'TICKETMASTER',
+            api: 'events',
             key: '1WLkNy3Qylx70A9ds5a5gXCT2PNoGeGq',
-            userSecret: '9iz1WGmScFHY3Wcj',
-            url: 'https://app.ticketmaster.com/discovery/v1/events.json?apikey='
+            url: 'https://app.ticketmaster.com/discovery/v1/events.apiResponse?apikey='
         }
     ];
     // < --- ========== End API KEYS ========== --- >
 
+    // --------------- DEPRECATED TESTING ---------------
     // < --- ========== BEGIN API CALL FUNCTION ========== --- >
-    let callAPI = function () {
-        var queryURL = apiKeys[i].url + apiKeys[i].key;
+    // let callAPI = function () {
+    //     var queryURL = apiKeys[i].url + apiKeys[i].key;
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET",
+    //         success: function (data) {
+    //             myVariable = data;
+    //         }
+    //         // dataType: 'jsonp',
+    //     }).then(function (response) {
+    //         // do a bunch of cool things with our returned apiResponse data here
+    //         console.log(response); // testing API response   
+    //     });
+    // } // end callAPI
+
+    // // loop thru api responses for easy access during dev and DRY code in production
+    // for (i = 0; i < apiKeys.length; i++) {
+    //     //callAPI(); // calling our API method to test
+    // }
+    // --------------- DEPRECATED TESTING ---------------
+
+    // < --- ========== BEGIN API SINGLE USE TESTING ========== --- >
+   
+
+    let callAPI_test = function (url, key, api) {
+        var queryURL = url + key;
+        name = api
         $.ajax({
             url: queryURL,
             method: "GET",
+            success: function (data) {
+                 apiResponse.push(data); // trying to dynamically create a variable name - dunno if it is possible
+            }
             // dataType: 'jsonp',
         }).then(function (response) {
-            // do a bunch of cool things with our returned json data here
-            console.log(response); // testing API response
-        }); // end ajax call
-        console.log((i + 1 + ' - ') + apiKeys[i].api);
-    } // end callAPI
+            
+            
+            // do a bunch of cool things with our returned apiResponse data here
+            //console.log(response); // testing API response   
+            //console.log(apiResponse); 
+        });
+    } // end callAPI_test
 
-    // loop thru api responses for easy access during dev and DRY code in production
-    for (i = 0; i < apiKeys.length; i++) {
-        callAPI(); // calling our API method to test
-    }
+callAPI_test(apiKeys[0].url, apiKeys[0].key);
+callAPI_test(apiKeys[1].url, apiKeys[1].key);
+
+console.log(apiResponse); 
+// < --- ========== END API SINGLE USE TESTING ========== --- >
+
 
     // just testing the magic css animation library
     $('.yourdiv').hover(function () {
