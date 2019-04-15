5ac5871a0dc4d78c4b581d93f49db00c1c5bf43a
John Robertson <tiktaalikdesign@gmail.com>
2019-04-15 19:32:36 -0400

    framing up the flow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     let latitude = ''; // this changes with each step in the app (initial ip geolocation, then dining, then event)
     let longitude = ''; // this changes with each step in the app (initial ip geolocation, then dining, then event)
+    let city = '';
+    let APIResponse = false;
+
 
     //    just set the info for the ticketmaster call as an object (temporary for testing)
     let apiEvent = {
@@ -24,18 +27,60 @@ $(document).ready(function () {
             method: "GET",
             // dataType: 'jsonp', // optional - needed to work around a CORS warning/error
         }).then(function (response) {
+            APIResponse = true;
             console.log(response); // returns the initial ip address geolocation data
             latitude = response.latitude; // sets current lat
             longitude = response.longitude; // sets current long
+            city = response.city;
             console.log('User latitude is ' + latitude); // tell us the lat
             console.log('User longitude is ' + longitude); // tell us the long
+            console.log('You are in ' + city);
         });
     };
     // ==================== End GeoLocation API Call ====================
+    let callAPIEvents = function () {
+        let url = 'https://app.ticketmaster.com/discovery/v2/events.json?city=';
+        const key = '1WLkNy3Qylx70A9ds5a5gXCT2PNoGeGq';
+        var queryURL = url + city + '&apikey=' + key;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            // dataType: 'jsonp', // optional - needed to work around a CORS warning/error
+        }).then(function (response) {
+            console.log(response); // returns the initial ip address geolocation data
+            for (i = 0; i < response._embedded.events.length; i++) {
+                console.log(response._embedded.events[i].name);
+                let event = response._embedded.events[i].name;
+                let image = response._embedded.events[i].images[5].url;
+                let imageEvent = $('<img>');
+                imageEvent.attr('src', image);
+                imageEvent.addClass('image');
+                let eventItem = $('<div>');
+                eventItem.text(event);
+                $('#eventsList').append(eventItem);
+                $('#eventsList').append(imageEvent);
+            }
+        });
+    };
+
 
     // ==================== click to start app - populate dining, movie, event buttons ====================
     $('#start').on('click', function () {
         callAPIGeo(); // gets the initial geolocation data for the user
-        $('.menu1-btn').addClass('scale-in'); // reveals the next menu options (these will eventually be in a seperate component)
+        if (APIResponse === true) {
+            $('.menu1-btn').addClass('scale-in'); // reveals the next menu options (these will eventually be in a seperate component)
+            $('#city').text('It looks like you are in ' + city + '.  Is this correct?'); // populates the text for location (depends on APIResponse === true)
+        } else {
+            alert('Loading data - please wait and try again...'); // placeholder for loading - set loading animation and error timeout modal
+        };
     });
+
+    $('#eventSearch').on('click', function() {
+        callAPIEvents();
+        
+    })
+
 });
+
+
+// _embedded.events[""0""]
