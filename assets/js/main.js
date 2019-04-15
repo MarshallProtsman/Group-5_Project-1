5dbe2d3a7fb0c1e570fcbd15d3a2c0fbf6416ff8
John Robertson <tiktaalikdesign@gmail.com>
2019-04-15 18:18:57 -0400

    backtracked and reset geolocation API call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,41 @@
-let apiResponse = [];
-let userZip = '';
-let choice = '';
-let key = '';
-
 $(document).ready(function () {
+    let latitude = ''; // this changes with each step in the app (initial ip geolocation, then dining, then event)
+    let longitude = ''; // this changes with each step in the app (initial ip geolocation, then dining, then event)
 
-    // < --- ==================== Begin API KEYS ========== ==========--- >
-    // placing our keys here so we can reuse ajax method
-    let apiKeys = [
-        {
-            api: 'geo',
-            key: '8bf6f7f9f28c7ed5495f6f6353d07d28a05c6bb9fbab598f32e7cba3',
-            params: [],
-            url: 'https://api.ipdata.co/?api-key=',
-        },
-        // extra template api object
-        // {
-        //     api: '',
-        //     key: '',
-        //     url: '',
-        // },
-        {
-            api: 'events',
-            zip: '',
-            key: '1WLkNy3Qylx70A9ds5a5gXCT2PNoGeGq',
-            url: 'https://app.ticketmaster.com/discovery/v1/events/postalCode' + userZip + '.apiResponse?apikey='
+    //    just set the info for the ticketmaster call as an object (temporary for testing)
+    let apiEvent = {
+        api: 'events',
+        zip: '',
+        country: 'US',
+        city: 'Atlanta',
+        key: '1WLkNy3Qylx70A9ds5a5gXCT2PNoGeGq',
+        get url() {
+            return 'https://app.ticketmaster.com/discovery/v2/events.json?city=' + this.city + '&apikey=';
         }
-    ];
-    // < --- ==================== End API KEYS ==================== --- >
-key = apiKeys;
-    // ------------------------------  DEPRECATED TESTING -------------------------- ----
-    // < --- ==================== BEGIN API CALL FUNCTION ==================== --- >
-    // let callAPI = function () {
-    //     var queryURL = apiKeys[i].url + apiKeys[i].key;
+    };
 
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET",
-    //         success: function (data) {
-    //             myVariable = data;
-    //         }
-    //         // dataType: 'jsonp',
-    //     }).then(function (response) {
-    //         // do a bunch of cool things with our returned apiResponse data here
-    //         console.log(response); // testing API response   
-    //     });
-    // } // end callAPI
-
-    // // loop thru api responses for easy access during dev and DRY code in production
-    // for (i = 0; i < apiKeys.length; i++) {
-    //     //callAPI(); // calling our API method to test
-    // }
-    // ------------------------ ------ DEPRECATED TESTING --------------------------- ---
-
-    // < --- ==================== BEGIN API SINGLE USE TESTING ==================== --- >
-
-
-    let callAPI_test = function (url, key) {
+    // ==================== Begin GeoLocation API Call ====================
+    let callAPIGeo = function () {
+        let url = 'https://api.ipdata.co/?api-key=';
+        const key = '8bf6f7f9f28c7ed5495f6f6353d07d28a05c6bb9fbab598f32e7cba3';
         var queryURL = url + key;
         $.ajax({
             url: queryURL,
             method: "GET",
-            success: function (data) {
-                apiResponse.push(data); // trying to dynamically create a variable name - dunno if it is possible
-            }
             // dataType: 'jsonp', // optional - needed to work around a CORS warning/error
         }).then(function (response) {
-            // do a bunch of cool things with our returned apiResponse data here
-            //console.log(response); // testing API response   
-            //console.log(apiResponse); 
+            console.log(response); // returns the initial ip address geolocation data
+            latitude = response.latitude; // sets current lat
+            longitude = response.longitude; // sets current long
+            console.log('User latitude is ' + latitude); // tell us the lat
+            console.log('User longitude is ' + longitude); // tell us the long
         });
-    } // end callAPI_test
+    };
+    // ==================== End GeoLocation API Call ====================
 
-    
-    callAPI_test(apiKeys[0].url, apiKeys[0].key);
-    
-
-    function userLocale() {
-        let zip = apiResponse[0].postal;
-        userZip = zip;
-        apiKeys[1].zip = zip;
-        $('#info1').text('User zip: ' + zip);
-    }
-
-    // userLocale();
-    // console.log(apiResponse);
-    // console.log(apiKeys);
-    // < --- ========== END API SINGLE USE TESTING ========== --- >
-
-// $('#start').on('click', function() {
-// $('#splash').css('opacity', '0');
-// $('#menu1').css('opacity', '1');
-// setTimeout(function() {
-// $('#splash').addClass('hidden');
-// $('#menu1').removeClass('hidden');
-// }, 1000)
-    
-// });
-
-$('#start').on('click', function() {
-    userLocale();
-    $('.menu1-btn').addClass('scale-in');
-    // $('.menu1-btn').removeClass('scale-out');
-})
-
-$('#event').on('click', function() {
-    callAPI_test(apiKeys[1].url, apiKeys[1].key);
-    console.log(apiResponse[1])
-})
-
-}) // end document.ready
-
-
-
-// --- ==================== API KEYS NOT YET IN APIKEYS ARRAY ==================== --- //
-/*
-
-/ ----------------------------------------------------------------- /
-IMDB - movies (1000 per day max)
-🔑 - 69b945e06fmsh34874c0e87f7ddcp13b6f5jsn04b50333f22b
-/ ----------------------------------------------------------------- /
-
-*/
-// < --- ==================== API KEYS NOT YET IN APIKEYS ARRAY ==================== --- > 
+    // ==================== click to start app - populate dining, movie, event buttons ====================
+    $('#start').on('click', function () {
+        callAPIGeo(); // gets the initial geolocation data for the user
+        $('.menu1-btn').addClass('scale-in'); // reveals the next menu options (these will eventually be in a seperate component)
+    });
+});
